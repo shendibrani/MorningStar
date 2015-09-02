7fad043127f29c65ca5363b60d04bd6ba9288aab
Benita Stärke <benitastaerke987@gmail.com>
2015-09-02 15:26:11 +0200

    Health Bar Greybox

---
+++ Assets/Scripts/HUD/HealthBar.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+	public float _health;
+	private float _maxhealth = 0;
+	private Image LifeBar;
+
+
+	// Use this for initialization
+	void Start () {
+
+		_maxhealth = _health;
+
+		LifeBar = this.gameObject.GetComponent<Image>();
+ 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	if (_health / _maxhealth >= 0){
+
+			if (Input.GetKeyDown (KeyCode.DownArrow)) {
+				
+				_health = _health - 1;
+
+			}
+
+			LifeBar.fillAmount = _health / _maxhealth;
+
+		}
+
+	}
+}
