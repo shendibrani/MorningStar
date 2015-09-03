5a01ac197297f2194fa32db22bf5cbf9608f34d5
Giulio Robecchi <giulio.robecchi@gmail.com>
2015-09-03 11:23:34 +0200

    Input Configured for 4 players, created the basic axis binder.

---
+++ Assets/Scripts/Control System/AnalogToAxisLayer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnalogToAxisLayer : Axis 
+{
+	public new float axisValue {
+		get{
+			if(focus){
+				string AxisName = "P"+joystick+" ";
+				if(direction == StickDirection.HORIZONTAL){
+					AxisName += "Horizontal ";
+				} else {
+					AxisName += "Vertical ";
+				}
+				if(type == StickType.MOVEMENT){
+					AxisName += "Movement";
+				} else {
+					AxisName += "Attack";
+				}
+				Input.GetAxis(AxisName);
+			}
+			return 0;
+		}
+	}
+
+	[SerializeField] int joystick;
+	[SerializeField] StickDirection direction;
+	[SerializeField] StickType type;
+}
+
+public enum StickType{
+	MOVEMENT, ATTACK
+}
+
+public enum StickDirection{
+	HORIZONTAL, VERTICAL
+}
