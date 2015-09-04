fa93e09816fd84f589158d76d9b7ed768d0288cc
TemplarWarden <daniel.louis.raynor@gmail.com>
2015-09-04 14:01:07 +0200

    Working on better image displays and events

---
+++ Assets/Scripts/HUD/ImageController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(ImageController))]
+public class ImageController : MonoBehaviour {
+
+
+    [SerializeField]
+    float fadeTimer;
+    [SerializeField]
+    float displayTimer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Enable()
+    {
+        GetComponent<ImageController>().enabled = true;
+    }
+
+    public void Disable()
+    {
+        GetComponent<ImageController>().enabled = false;
+    }
+
+    public void FadeOut()
+    {
+
+    }
+
+    public void FadeIt()
+    {
+
+    }
+}
