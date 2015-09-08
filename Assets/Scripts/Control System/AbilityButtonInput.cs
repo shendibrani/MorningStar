40dfc974782eb56b5ea9b36574ba7c350db62dae
TemplarWarden <daniel.louis.raynor@gmail.com>
2015-09-08 12:09:37 +0200

    Added Ability to character controller, using execute commands on key/axis bindings

---
+++ Assets/Scripts/Control System/AbilityButtonInput.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbilityButtonInput : KeyboardToAxisLayer {
+
+    [SerializeField]
+    Ability plusAbility;
+    [SerializeField]
+    Ability minusAbility;
+
+    bool hasExecuted = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+    void Update()
+    {
+        base.Update();
+        
+        if (axisValue == 1 && !hasExecuted)
+        {
+            plusAbility.Execute();
+            hasExecuted = true;
+        }
+        else if (axisValue == -1 && !hasExecuted) 
+        {
+            minusAbility.Execute();
+            hasExecuted = true;
+        }
+        else if (axisValue != 1 && axisValue != -1)
+        {
+            hasExecuted = false;
+        }
+       
+       
+	}
+}
---
+++ Assets/Scripts/DashAbility.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DashAbility : Ability {
+
+    Transform other;
+
+    [SerializeField]
+    Axis upDown;
+    [SerializeField]
+    Axis leftRight;
+    [SerializeField]
+    float dashPower;
+    [SerializeField]
+    float interval = 1f;
+
+    float timer = 0;
+
+	// Use this for initialization
+	void Start () {
+        List<LookAtEachotherBehaviour> list = new List<LookAtEachotherBehaviour>(FindObjectsOfType<LookAtEachotherBehaviour>());
+        other = list.Find(x => x != this).transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public override void Execute()
+    {
+        if (Time.time >= timer)
+        {
+            Debug.Log(leftRight.axisValue);
+            Debug.Log(upDown.axisValue);
+            if (leftRight.axisValue != 0 && upDown.axisValue != 0)
+            {
+                Debug.Log("Aimed");
+                GetComponent<Rigidbody>().velocity = (new Vector3(leftRight.axisValue, 0, upDown.axisValue)).normalized * dashPower * Time.deltaTime;
+            }
+            else
+            {
+                Debug.Log("Auto");
+                Vector3 difference = this.transform.position - other.transform.position;
+                GetComponent<Rigidbody>().velocity = (new Vector3(difference.x, 0, difference.y)).normalized * dashPower * Time.deltaTime;
+            }
+            timer = interval + Time.time;
+        }
+
+    }
+}
