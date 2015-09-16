9433fc8882fbdb86b676fda601f050e29a301c1d
TemplarWarden <daniel.louis.raynor@gmail.com>
2015-09-16 12:56:48 +0200

    Worked on firewall

---
+++ Assets/Scripts/FireWallController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireWallController : MonoBehaviour,IMessage {
+
+    [SerializeField]
+    float wallSpeed;
+    [SerializeField]
+    float maxMotionDistance;
+
+    float distance = 0;
+
+	// Use this for initialization
+	void Start () {
+        MessagingManager.AddListener(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (distance < maxMotionDistance)
+        {
+            transform.position += transform.forward * Time.deltaTime * wallSpeed;
+            distance += Time.deltaTime * wallSpeed;
+        }
+
+	}
+
+    public void Message(Messages message, GameObject sender)
+    {
+        switch (message)
+        {
+            case Messages.DEATH:
+                StopMotion();
+                break;
+            case Messages.RESTART:
+                ResetMotion();
+                break;
+        }
+    }
+
+    void ResetMotion()
+    {
+        distance = 0;
+    }
+
+    void StopMotion()
+    {
+        distance = maxMotionDistance + 1;
+    }
+}
