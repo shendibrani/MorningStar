134fc20130a04a916c13f6205c3f3cd39bcd0ed8
TemplarWarden <daniel.louis.raynor@gmail.com>
2015-09-16 10:58:43 +0200

    Working on Pit and Firewall

---
+++ Assets/Scripts/FireWallKnockback.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireWallKnockback : MonoBehaviour {
+
+    [SerializeField]
+    float force = 300f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision c)
+    {
+        if (c.gameObject.GetComponent<Rigidbody>()) c.gameObject.GetComponent<Rigidbody>().AddForce(this.transform.forward * force, ForceMode.Impulse);
+    }
+}
