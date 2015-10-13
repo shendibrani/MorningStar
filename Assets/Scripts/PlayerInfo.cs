80d6559808b157e93d66e5e8d7dfa2dad9dd96bb
TemplarWarden <daniel.louis.raynor@gmail.com>
2015-10-13 11:02:25 +0200

    Finished input manager, set up player info

--- Assets/Scripts/PlayerInfo.cs
+++ Assets/Scripts/PlayerInfo.cs
@@ -53,63 +53,70 @@ public class PlayerInfo : MonoBehaviour, IMessage {
     {
         playerID = id;
 
-//        foreach (AnalogToAxisLayer analog in GetComponentsInChildren<AnalogToAxisLayer>())
-//        {
-//            analog.player = playerID;
-//			if (analog.type == StickType.MOVEMENT){
-//				if(analog.direction == StickDirection.HORIZONTAL){
-//					analog.axisName = movementAxisX.axisName;
-//					analog.invert = movementAxisX.invert;
-//				} else {
-//					analog.axisName = movementAxisY.axisName;
-//					analog.invert = movementAxisY.invert;
-//				}
-//			} else if (analog.type == StickType.ATTACK){
-//				if(analog.direction == StickDirection.HORIZONTAL){
-//					analog.axisName = attackAxisX.axisName;
-//					analog.invert = attackAxisX.invert;
-//				} else {
-//					analog.axisName = attackAxisY.axisName;
-//					analog.invert = attackAxisY.invert;
-//				}
-//			}
-//            if (analog.type == StickType.TRIGGER)
-//            {
-//                analog.axisName = "J" + (playerID + 1) + " Triggers";
-//            }
-//        }
-//        foreach (AnalogToAxisLayer analog in rightRotator.GetComponentsInParent<AnalogToAxisLayer>())
-//        {
-//            analog.player = playerID;
-//            if (analog.type == StickType.MOVEMENT)
-//            {
-//                if (analog.direction == StickDirection.HORIZONTAL)
-//                {
-//                    Debug.Log(movementAxisX.axisName);
-//                    analog.axisName = movementAxisX.axisName;
-//                    analog.invert = movementAxisX.invert;
-//                }
-//                else
-//                {
-//                    Debug.Log(movementAxisY.axisName);
-//                    analog.axisName = movementAxisY.axisName;
-//                    analog.invert = movementAxisY.invert;
-//                }
-//            }
-//            else if (analog.type == StickType.ATTACK)
-//            {
-//                if (analog.direction == StickDirection.HORIZONTAL)
-//                {
-//                    analog.axisName = attackAxisX.axisName;
-//                    analog.invert = attackAxisX.invert;
-//                }
-//                else
-//                {
-//                    analog.axisName = attackAxisY.axisName;
-//                    analog.invert = attackAxisY.invert;
-//                }
-//            }
-//        }
+        foreach (AnalogToAxisLayer analog in GetComponentsInChildren<AnalogToAxisLayer>())
+        {
+            analog.player = playerID;
+            if (analog.type == StickType.MOVEMENT)
+            {
+                if (analog.direction == StickDirection.HORIZONTAL)
+                {
+                    analog.axisName = controller.movement.array[0];
+                    //analog.invert = movementAxisX.invert;
+                }
+                else
+                {
+                    analog.axisName = controller.movement.array[1];
+                    //analog.invert = movementAxisY.invert;
+                }
+            }
+            else if (analog.type == StickType.ATTACK)
+            {
+                if (analog.direction == StickDirection.HORIZONTAL)
+                {
+                    analog.axisName = controller.attack.array[0];
+                    //analog.invert = attackAxisX.invert;
+                }
+                else
+                {
+                    analog.axisName = controller.attack.array[1]; ;
+                    //analog.invert = attackAxisY.invert;
+                }
+            }
+            if (analog.type == StickType.TRIGGER)
+            {
+                analog.axisName = controller.triggers.axisName;
+            }
+        }
+        foreach (AnalogToAxisLayer analog in rightRotator.GetComponentsInParent<AnalogToAxisLayer>())
+        {
+            analog.player = playerID;
+            if (analog.type == StickType.MOVEMENT)
+            {
+                if (analog.direction == StickDirection.HORIZONTAL)
+                {
+                    analog.axisName = controller.movement.array[0];
+                    //analog.invert = movementAxisX.invert;
+                }
+                else
+                {
+                    analog.axisName = controller.movement.array[1];
+                    //analog.invert = movementAxisY.invert;
+                }
+            }
+            else if (analog.type == StickType.ATTACK)
+            {
+                if (analog.direction == StickDirection.HORIZONTAL)
+                {
+                    analog.axisName = controller.attack.array[0];
+                    //analog.invert = attackAxisX.invert;
+                }
+                else
+                {
+                    analog.axisName = controller.attack.array[1]; ;
+                    //analog.invert = attackAxisY.invert;
+                }
+            }
+        }
     }
 
     public void AttachWeapon(GameObject weapon)
